687c3f13d755c81de8ab6e5f58eef3a5dae8f8d2
Fabrice Niyomwungeri <niomwungeri.fabrice@gmail.com>
2024-11-04 11:49:12 -0700

    Initial commit

---
+++ test/poc-java21-builder-ecr.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as PocJava21BuilderEcr from '../lib/poc-java21-builder-ecr-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/poc-java21-builder-ecr-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new PocJava21BuilderEcr.PocJava21BuilderEcrStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
